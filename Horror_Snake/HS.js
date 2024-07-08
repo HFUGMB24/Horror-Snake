2148fa05c527d969015963cceba1088245d7f315
sheeshkebap <andr.niederreiter@gmail.com>
2024-07-08 14:00:09 +0200

    gg

--- Horror_Snake/HS.js
+++ Horror_Snake/HS.js
@@ -4,8 +4,6 @@ const ctx = canvas.getContext("2d");
 function drawGrid(width, height, rows, cols) {
     const cellWidth = 25;
     const cellHeight = 25;
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 1;
     for (let i = 0; i <= cols; i++) {
         let cell = {
             positionX: 0,
@@ -14,20 +12,17 @@ function drawGrid(width, height, rows, cols) {
             height: cellHeight,
             width: cellWidth,
         };
+        ctx.fillStyle = "red";
         cell.positionX = i * cellWidth;
         let rect = new Path2D();
         rect.rect(cell.positionX, cell.positionY, cellHeight, cellWidth);
-    }
-    for (let j = 0; j <= rows; j++) {
-        const y = j * cellHeight;
-        ctx.beginPath();
-        ctx.moveTo(0, y);
-        ctx.lineTo(width, y);
-        ctx.stroke();
-    }
-    for (let i = 0; i < 400; i++) {
-        grid.push(class {
-        });
+        ctx.fill(rect);
+        for (let j = 0; j <= rows; j++) {
+            cell.positionX = j * cellHeight;
+            rect.rect(cell.positionX, cell.positionY, cellHeight, cellHeight);
+            ctx.fill(rect);
+            grid.push(cell);
+        }
     }
 }
 let grid = [];
--- Horror_Snake/HS.ts
+++ Horror_Snake/HS.ts
@@ -5,11 +5,6 @@ function drawGrid(width: number, height: number, rows: number, cols: number) {
     const cellWidth = 25;
     const cellHeight = 25;
 
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 1;
-
-    
-
     for (let i = 0; i <= cols; i++) {
         let cell:cellData = {
             positionX:0,
@@ -19,25 +14,21 @@ function drawGrid(width: number, height: number, rows: number, cols: number) {
             width:cellWidth,
         }
 
+        ctx.fillStyle = "red";
+
         cell.positionX = i * cellWidth;
-        
         let rect: Path2D = new Path2D()
         rect.rect(cell.positionX, cell.positionY, cellHeight, cellWidth);
+        ctx.fill(rect);
+
+        for (let j = 0; j <= rows; j++) {
+            cell.positionX = j * cellHeight;
+            rect.rect(cell.positionX, cell.positionY, cellHeight, cellHeight);
+            ctx.fill(rect);
+
+            grid.push(cell)
+        }
  
-    }
-
-    for (let j = 0; j <= rows; j++) {
-        const y = j * cellHeight;
-        ctx.beginPath();
-        ctx.moveTo(0, y);
-        ctx.lineTo(width, y);
-        ctx.stroke();
-    }
-
-    for (let i = 0; i < 400; i++){
-        grid.push(
-            class
-        )
     }
     
 }
