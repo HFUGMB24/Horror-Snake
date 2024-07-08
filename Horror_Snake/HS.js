a5297a5cfad5b9e122fbb3220a9edcc848eb3e9c
M00Star <bjoern.martens@posteo.de>
2024-07-08 13:57:18 +0200

    add branch for vignette

--- Horror_Snake/HS.js
+++ Horror_Snake/HS.js
@@ -1,3 +1,18 @@
-"use strict";
-const canvas = document.getElementsByTagName("canvas")[0];
-const ctx = canvas.getContext("2d");
+var canvas = document.getElementsByTagName("canvas")[0];
+var ctx = canvas.getContext("2d");
+var viewDistance = 500;
+var playerX = 1000;
+var playerY = 1000;
+var bg = new Path2D();
+bg.rect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle = "rgb(255, 255, 255)";
+ctx.fill(bg);
+function drawVignette() {
+    var gradient = ctx.createRadialGradient(playerX, playerY, 0, playerX, playerY, viewDistance);
+    gradient.addColorStop(0.2, "rgba(0,0,0,0)");
+    gradient.addColorStop(1, "rgba(0,0,0,1)");
+    ctx.fillStyle = gradient;
+    var vignette = new Path2D();
+    vignette.rect(0, 0, canvas.width, canvas.height);
+    ctx.fill(vignette);
+}
--- Horror_Snake/HS.ts
+++ Horror_Snake/HS.ts
@@ -1,2 +1,26 @@
 const canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
 const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
+
+let viewDistance = 500;
+let playerX = 1000;
+let playerY = 1000;
+
+
+let bg: Path2D = new Path2D();
+bg.rect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle = "rgb(255, 255, 255)";
+ctx.fill(bg);
+
+function drawVignette() {
+    let gradient = ctx.createRadialGradient(playerX, playerY, 0, playerX, playerY, viewDistance);
+    gradient.addColorStop(0.2, "rgba(0,0,0,0)");
+    gradient.addColorStop(1, "rgba(0,0,0,1)");
+
+    ctx.fillStyle = gradient;
+    let vignette: Path2D = new Path2D();
+    vignette.rect(0, 0, canvas.width, canvas.height);
+
+    ctx.fill(vignette);
+}
+
+
