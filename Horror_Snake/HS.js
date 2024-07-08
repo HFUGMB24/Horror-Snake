03e4794a3df1718643e5e6cfbc7abb84c00311c0
sheeshkebap <andr.niederreiter@gmail.com>
2024-07-08 14:33:35 +0200

    Hello

--- Horror_Snake/HS.js
+++ Horror_Snake/HS.js
@@ -1,23 +1,24 @@
-var canvas = document.getElementsByTagName("canvas")[0];
-var ctx = canvas.getContext("2d");
+"use strict";
+const canvas = document.getElementsByTagName("canvas")[0];
+const ctx = canvas.getContext("2d");
 function drawGrid(width, height, rows, cols) {
-    var cellWidth = 25;
-    var cellHeight = 25;
+    const cellWidth = 25;
+    const cellHeight = 25;
     ctx.strokeStyle = 'white';
     ctx.fillStyle = "rgb(0, 0, 0)";
     ctx.lineWidth = 1;
-    for (var i = 0; i <= cols; i++) {
-        var x = i * cellWidth;
-        for (var j = 0; j <= rows; j++) {
-            var y = j * cellHeight;
-            var cell = {
+    for (let i = 0; i <= cols; i++) {
+        let x = i * cellWidth;
+        for (let j = 0; j <= rows; j++) {
+            let y = j * cellHeight;
+            let cell = {
                 positionX: x,
                 positionY: y,
                 class: "",
                 height: cellHeight,
                 width: cellWidth,
             };
-            var rect = new Path2D();
+            let rect = new Path2D();
             rect.rect(cell.positionX, cell.positionY, cellHeight, cellWidth);
             ctx.fill(rect);
             ctx.stroke(rect);
@@ -25,6 +26,6 @@ function drawGrid(width, height, rows, cols) {
         }
     }
 }
-var grid = [];
+let grid = [];
 drawGrid(canvas.width, canvas.height, 18, 32);
 console.log(grid);
